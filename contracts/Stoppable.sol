f374bd019c3a552b8c4567cd4f69f796308a37b0
Federico Bond <federicobond@gmail.com>
2016-11-01 23:34:57 -0300

    Make Stoppable a subclass of Ownable. Fixes #47

--- contracts/Stoppable.sol
+++ contracts/Stoppable.sol
@@ -1,28 +1,24 @@
 pragma solidity ^0.4.0;
+
+import "./Ownable.sol";
 /*
  * Stoppable
  * Abstract contract that allows children to implement an
  * emergency stop mechanism.
  */
-contract Stoppable {
-  address public curator;
+contract Stoppable is Ownable {
   bool public stopped;
 
   modifier stopInEmergency { if (!stopped) _; }
   modifier onlyInEmergency { if (stopped) _; }
 
-  function Stoppable(address _curator) {
-    if (_curator == 0) throw;
-    curator = _curator;
-  }
-
   function emergencyStop() external {
-    if (msg.sender != curator) throw;
+    if (msg.sender != owner) throw;
     stopped = true;
   }
 
   function release() external onlyInEmergency {
-    if (msg.sender != curator) throw;
+    if (msg.sender != owner) throw;
     stopped = false;
   }
 
